3cddf93d1bd81a653d2a13eba858a246369cea81
guoyang <809525536@qq.com>
2021-06-30 20:32:11 +0800

    change

--- src/router/index.js
+++ src/router/index.js
@@ -69,56 +69,56 @@ export const constantRoutes = [{
       icon: 'dashboard'
     }
   },
-  {
-    path: '/safetyMonitor/warning',
-    name: '报警统计分析',
-    component: () => import('@/views/warning/index'),
-    meta: {
-      title: '报警统计分析',
-      icon: 'dashboard'
-    }
-  },
-  {
-    path: '/safetyMonitor/advanceWarning',
-    name: '预告警管理',
-    component: () => import('@/views/advanceWarning/index'),
-    meta: {
-      title: '预告警管理',
-      icon: 'dashboard'
-    }
-  },
+  // {
+  //   path: '/safetyMonitor/warning',
+  //   name: '报警统计分析',
+  //   component: () => import('@/views/warning/index'),
+  //   meta: {
+  //     title: '报警统计分析',
+  //     icon: 'dashboard'
+  //   }
+  // },
+  // {
+  //   path: '/safetyMonitor/advanceWarning',
+  //   name: '预告警管理',
+  //   component: () => import('@/views/advanceWarning/index'),
+  //   meta: {
+  //     title: '预告警管理',
+  //     icon: 'dashboard'
+  //   }
+  // },
   ]
 },
 
-{
-  path: '/energy',
-  component: Layout,
-  name: '能源管理中心',
-  redirect: '/energy/comprehensiveEnergy',
-  meta: {
-    title: '预告警管理',
-    icon: 'el-icon-s-opportunity'
-  },
-  children: [{
-    path: '/energy/comprehensiveEnergy',
-    name: '综合用能管理',
-    component: () => import('@/views/comprehensiveEnergy/index'),
-    meta: {
-      title: '综合用能管理',
-      icon: 'el-icon-s-opportunity'
-    }
-  },
-  {
-    path: '/energy/electricityManagement',
-    name: '用电管理',
-    component: () => import('@/views/electricityManagement/index'),
-    meta: {
-      title: '用电管理',
-      icon: 'el-icon-s-opportunity'
-    }
-  }
-  ]
-},
+// {
+//   path: '/energy',
+//   component: Layout,
+//   name: '能源管理中心',
+//   redirect: '/energy/comprehensiveEnergy',
+//   meta: {
+//     title: '预告警管理',
+//     icon: 'el-icon-s-opportunity'
+//   },
+//   children: [{
+//     path: '/energy/comprehensiveEnergy',
+//     name: '综合用能管理',
+//     component: () => import('@/views/comprehensiveEnergy/index'),
+//     meta: {
+//       title: '综合用能管理',
+//       icon: 'el-icon-s-opportunity'
+//     }
+//   },
+//   {
+//     path: '/energy/electricityManagement',
+//     name: '用电管理',
+//     component: () => import('@/views/electricityManagement/index'),
+//     meta: {
+//       title: '用电管理',
+//       icon: 'el-icon-s-opportunity'
+//     }
+//   }
+//   ]
+// },
 
 // {
 //   path: '/dashboard',
@@ -132,35 +132,35 @@ export const constantRoutes = [{
 //   }]
 // },
 
-{
-  path: '/example',
-  component: Layout,
-  redirect: '/example/table',
-  name: 'Example',
-  meta: {
-    title: 'Example',
-    icon: 'el-icon-s-help'
-  },
-  children: [{
-    path: 'table',
-    name: 'Table',
-    component: () => import('@/views/table/index'),
-    meta: {
-      title: 'Table',
-      icon: 'table'
-    }
-  },
-  {
-    path: 'tree',
-    name: 'Tree',
-    component: () => import('@/views/tree/index'),
-    meta: {
-      title: 'Tree',
-      icon: 'tree'
-    }
-  }
-  ]
-},
+// {
+//   path: '/example',
+//   component: Layout,
+//   redirect: '/example/table',
+//   name: 'Example',
+//   meta: {
+//     title: 'Example',
+//     icon: 'el-icon-s-help'
+//   },
+//   children: [{
+//     path: 'table',
+//     name: 'Table',
+//     component: () => import('@/views/table/index'),
+//     meta: {
+//       title: 'Table',
+//       icon: 'table'
+//     }
+//   },
+//   {
+//     path: 'tree',
+//     name: 'Tree',
+//     component: () => import('@/views/tree/index'),
+//     meta: {
+//       title: 'Tree',
+//       icon: 'tree'
+//     }
+//   }
+//   ]
+// },
 
 {
   path: '/eleReport',
@@ -270,101 +270,101 @@ export const constantRoutes = [{
 
 
 
-{
-  path: '/form',
-  component: Layout,
-  children: [{
-    path: 'index',
-    name: 'Form',
-    component: () => import('@/views/form/index'),
-    meta: {
-      title: 'Form',
-      icon: 'form'
-    }
-  }]
-},
+// {
+//   path: '/form',
+//   component: Layout,
+//   children: [{
+//     path: 'index',
+//     name: 'Form',
+//     component: () => import('@/views/form/index'),
+//     meta: {
+//       title: 'Form',
+//       icon: 'form'
+//     }
+//   }]
+// },
 
-{
-  path: '/nested',
-  component: Layout,
-  redirect: '/nested/menu1',
-  name: 'Nested',
-  meta: {
-    title: 'Nested',
-    icon: 'nested'
-  },
-  children: [{
-    path: 'menu1',
-    component: () => import('@/views/nested/menu1/index'), // Parent router-view
-    name: 'Menu1',
-    meta: {
-      title: 'Menu1'
-    },
-    children: [{
-      path: 'menu1-1',
-      component: () => import('@/views/nested/menu1/menu1-1'),
-      name: 'Menu1-1',
-      meta: {
-        title: 'Menu1-1'
-      }
-    },
-    {
-      path: 'menu1-2',
-      component: () => import('@/views/nested/menu1/menu1-2'),
-      name: 'Menu1-2',
-      meta: {
-        title: 'Menu1-2'
-      },
-      children: [{
-        path: 'menu1-2-1',
-        component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-        name: 'Menu1-2-1',
-        meta: {
-          title: 'Menu1-2-1'
-        }
-      },
-      {
-        path: 'menu1-2-2',
-        component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-        name: 'Menu1-2-2',
-        meta: {
-          title: 'Menu1-2-2'
-        }
-      }
-      ]
-    },
-    {
-      path: 'menu1-3',
-      component: () => import('@/views/nested/menu1/menu1-3'),
-      name: 'Menu1-3',
-      meta: {
-        title: 'Menu1-3'
-      }
-    }
-    ]
-  },
-  {
-    path: 'menu2',
-    component: () => import('@/views/nested/menu2/index'),
-    name: 'Menu2',
-    meta: {
-      title: 'menu2'
-    }
-  }
-  ]
-},
+// {
+//   path: '/nested',
+//   component: Layout,
+//   redirect: '/nested/menu1',
+//   name: 'Nested',
+//   meta: {
+//     title: 'Nested',
+//     icon: 'nested'
+//   },
+//   children: [{
+//     path: 'menu1',
+//     component: () => import('@/views/nested/menu1/index'), // Parent router-view
+//     name: 'Menu1',
+//     meta: {
+//       title: 'Menu1'
+//     },
+//     children: [{
+//       path: 'menu1-1',
+//       component: () => import('@/views/nested/menu1/menu1-1'),
+//       name: 'Menu1-1',
+//       meta: {
+//         title: 'Menu1-1'
+//       }
+//     },
+//     {
+//       path: 'menu1-2',
+//       component: () => import('@/views/nested/menu1/menu1-2'),
+//       name: 'Menu1-2',
+//       meta: {
+//         title: 'Menu1-2'
+//       },
+//       children: [{
+//         path: 'menu1-2-1',
+//         component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
+//         name: 'Menu1-2-1',
+//         meta: {
+//           title: 'Menu1-2-1'
+//         }
+//       },
+//       {
+//         path: 'menu1-2-2',
+//         component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
+//         name: 'Menu1-2-2',
+//         meta: {
+//           title: 'Menu1-2-2'
+//         }
+//       }
+//       ]
+//     },
+//     {
+//       path: 'menu1-3',
+//       component: () => import('@/views/nested/menu1/menu1-3'),
+//       name: 'Menu1-3',
+//       meta: {
+//         title: 'Menu1-3'
+//       }
+//     }
+//     ]
+//   },
+//   {
+//     path: 'menu2',
+//     component: () => import('@/views/nested/menu2/index'),
+//     name: 'Menu2',
+//     meta: {
+//       title: 'menu2'
+//     }
+//   }
+//   ]
+// },
 
-{
-  path: 'external-link',
-  component: Layout,
-  children: [{
-    path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-    meta: {
-      title: 'External Link',
-      icon: 'link'
-    }
-  }]
-},
+// {
+//   path: 'external-link',
+//   component: Layout,
+//   children: [{
+//     path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
+//     meta: {
+//       title: 'External Link',
+//       icon: 'link'
+//     }
+//   }]
+// },
 
 // 404 page must be placed at the end !!!
 {
