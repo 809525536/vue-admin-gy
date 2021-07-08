fc4944b0953c3186455e1bd7cda3c112fe5fa70d
809525536 <809525536@qq.com>
2021-07-08 17:47:34 +0800

    思维导图

--- src/views/safetyMonitor/index.vue
+++ src/views/safetyMonitor/index.vue
@@ -5,7 +5,7 @@
         <el-card class="box-card">
           <div slot="header" class="clearfix">
             <span>用能统计</span>
-            <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+            <!-- <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button> -->
           </div>
           <div class="list">
             <div class="list-data">
